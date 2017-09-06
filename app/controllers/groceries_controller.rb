ddd6fed5b238a71471688cdab42f6c6200aae4ad
Timothy Meixell <misternu@gmail.com>
2017-09-06 13:37:49 -0500

    Complete MVP

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -1,16 +1,25 @@
 class GroceriesController < ApplicationController
   def index
     @grocery = Grocery.new
-    @groceries = Grocery.all.reverse
+    @groceries = Grocery.all.order(:id).reverse
   end
 
   def create
     @grocery = Grocery.create(grocery_params)
   end
 
+  def update
+    Grocery.find(params[:id]).update(grocery_params)
+  end
+
   def destroy
     @id = params[:id]
     Grocery.destroy(@id)
+  end
+
+  def clear
+    Grocery.where(checked: true).destroy_all
+    @groceries = Grocery.all.order(:id).reverse
   end
 
   private
